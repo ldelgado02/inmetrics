2c4d9ad0a0a1bc766947dacd7761ca178984067d
Luis Gustavo Delgado <luisgustavo220602@gmail.com>
2026-07-20 19:54:58 -0300

    Adição relatorio Allure aos testes

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,20 @@
 const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
+const browserify = require("@cypress/browserify-preprocessor");
+const { allureCypress } = require("allure-cypress/reporter");
+
+const options = browserify.defaultOptions;
+options.browserifyOptions.transform[1][1].global = true;
+options.browserifyOptions.transform[1][1].ignore = [/node_modules\/(?!allure-cypress)/];
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://www.automationexercise.com",
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
-      on("file:preprocessor", cucumber());
+      on("file:preprocessor", cucumber(options));
+      allureCypress(on, config);
+      return config;
     },
   },
 });
